f1cd861b73599ec1cefbc5bc97430140c4141ca1
Breon White <bdwhite92@gmail.com>
2021-11-28 15:32:03 -0500

    commit tree 2ecc9f4ab747113d80a2e427d7de268affc130ed

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(miles) {
+  let feetGreeting;
+  if (miles > 2500) {
+    feetGreeting = `No can do.`;
+  } else if (miles >= 2000) {
+      feetGreeting = `I will gladly take your thirty bucks.`;
+    } else if (miles <= 400) {
+        feetGreeting = `This one is on me!`;
+      }
+  return feetGreeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let checkCity;
+  city === `NYC` ? (checkCity = `Ok, sounds good.`) : (checkCity = `No go.`);
+  return checkCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  let tipResponse;
+  
+  switch (tipAmount) {
+    case `generous`:
+      tipResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipResponse = `Thank you.`;
+      break;
+    default: 
+      tipResponse = `Bye.`;
+      break;
+  }
+  return tipResponse;
 }
